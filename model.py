ad93027634b10bc0706d5206b5e93ee861a71cd0
Monisha <129486568+MonishaKarthikeyan07@users.noreply.github.com>
2024-04-23 11:11:14 +0530

    model.py

--- model.py
+++ model.py
@@ -1,32 +1,46 @@
+'''
+Author:Xuelei Chen(chenxuelei@hotmail.com)
+'''
 import torch
 import numpy as np
 import torch.nn as nn
+
 
 class AConvBlock(nn.Module):
     def __init__(self):
         super(AConvBlock,self).__init__()
 
-        block = [nn.Conv2d(3, 64, 3, padding=1), nn.PReLU(), nn.BatchNorm2d(64)]
-        block += [nn.Conv2d(64, 64, 3, padding=1), nn.PReLU(), nn.BatchNorm2d(64)]
+        block = [nn.Conv2d(3,3,3,padding = 1)]
+        block += [nn.PReLU()]
+
+        block += [nn.Conv2d(3,3,3,padding = 1)]
+        block += [nn.PReLU()]
+
         block += [nn.AdaptiveAvgPool2d((1,1))]
-        block += [nn.Conv2d(64, 32, 1), nn.PReLU(), nn.BatchNorm2d(32)]
-        block += [nn.Conv2d(32, 32, 1), nn.PReLU(), nn.BatchNorm2d(32)]
+        block += [nn.Conv2d(3,3,1)]
+        block += [nn.PReLU()]
+        block += [nn.Conv2d(3,3,1)]
+        block += [nn.PReLU()]
         self.block = nn.Sequential(*block)
 
-    def forward(self, x):
+    def forward(self,x):
         return self.block(x)
 
 class tConvBlock(nn.Module):
     def __init__(self):
         super(tConvBlock,self).__init__()
 
-        block = [nn.Conv2d(6, 8, 3, padding=1), nn.PReLU(), nn.BatchNorm2d(8)]
-        block += [nn.Conv2d(8, 8, 3, padding=2, dilation=2), nn.PReLU(), nn.BatchNorm2d(8)]
-        block += [nn.Conv2d(8, 8, 3, padding=5, dilation=5), nn.PReLU(), nn.BatchNorm2d(8)]
-        block += [nn.Conv2d(8, 3, 3, padding=1), nn.PReLU(), nn.BatchNorm2d(3)]
-        self.block = nn.Sequential(*block)
+        block = [nn.Conv2d(6,8,3,padding=1,dilation=1)]
+        block += [nn.PReLU()]
+        block += [nn.Conv2d(8,8,3,padding=2,dilation=2)]
+        block += [nn.PReLU()]
+        block += [nn.Conv2d(8,8,3,padding=5,dilation=5)]
+        block += [nn.PReLU()]
 
-    def forward(self, x):
+        block += [nn.Conv2d(8,3,3,padding=1)]
+        block += [nn.PReLU()]
+        self.block = nn.Sequential(*block)
+    def forward(self,x):
         return self.block(x)
 
 class PhysicalNN(nn.Module):
@@ -40,4 +54,4 @@ class PhysicalNN(nn.Module):
         A = self.ANet(x)
         t = self.tNet(torch.cat((x*0+A,x),1))
         out = ((x-A)*t + A)
-        return torch.clamp(out, 0., 1.)
+        return torch.clamp(out,0.,1.)
