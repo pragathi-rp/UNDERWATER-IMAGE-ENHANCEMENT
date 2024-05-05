9306ba9db82b908fd3a5787fba7a3d52640cea64
Pragatheeswari K <76157695+pragathi-rp@users.noreply.github.com>
2024-05-05 22:14:13 +0530

    uwcc.py

--- uwcc.py
+++ uwcc.py
@@ -4,8 +4,12 @@ from PIL import Image
 from torchvision import transforms
 
 def img_loader(path):
-    img = Image.open(path)
-    return img
+    try:
+        img = Image.open(path)
+        return img
+    except Exception as e:
+        print(f"Error loading image from {path}: {e}")
+        return None
 
 def get_imgs_list(ori_dirs, ucc_dirs):
     img_list = []
@@ -18,9 +22,9 @@ def get_imgs_list(ori_dirs, ucc_dirs):
 
     return img_list
 
-class uwcc(data.Dataset):
+class UWCCDataset(data.Dataset):
     def __init__(self, ori_dirs, ucc_dirs, train=True, loader=img_loader):
-        super(UWCCDataset, self).__init__()  # Fix the class name here
+        super(UWCCDataset, self).__init__()
 
         self.img_list = get_imgs_list(ori_dirs, ucc_dirs)
         if len(self.img_list) == 0:
@@ -29,33 +33,22 @@ class uwcc(data.Dataset):
         self.train = train
         self.loader = loader
 
-        if self.train:
-            print(f'Found {len(self.img_list)} pairs of training images')
-        else:
-            print(f'Found {len(self.img_list)} pairs of testing images')
-            
+        print(f"Found {len(self.img_list)} pairs of {'training' if train else 'testing'} images")
+
+        self.transform = transforms.Compose([
+            transforms.Resize((480, 640)),  # Resize images to a consistent size
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  # Adjust normalization as needed
+        ])
+
     def __getitem__(self, index):
         img_paths = self.img_list[index]
         sample = [self.loader(img_paths[i]) for i in range(len(img_paths))]
 
-        if self.train:
-            oritransform = transforms.Compose([
-                transforms.ToTensor(),
-            ])
-            ucctransform = transforms.Compose([
-                transforms.ToTensor(),
-            ])
-            sample[0] = oritransform(sample[0])
-            sample[1] = ucctransform(sample[1])
-        else:
-            oritransform = transforms.Compose([
-                transforms.ToTensor(),
-            ])
-            ucctransform = transforms.Compose([
-                transforms.ToTensor(),
-            ])
-            sample[0] = oritransform(sample[0])
-            sample[1] = ucctransform(sample[1])
+        if None in sample:
+            return None
+
+        sample = [self.transform(sample[0]), self.transform(sample[1])]
 
         return sample
 
